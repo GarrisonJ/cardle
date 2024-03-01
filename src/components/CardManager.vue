f7da8c604504fcb3dafa6235942440f877997046
Garrison Jensen <garrison.jensen@gmail.com>
2024-02-29 19:38:26 -0800

    Animate the other packs disapearing

--- src/components/CardManager.vue
+++ src/components/CardManager.vue
@@ -1,12 +1,13 @@
 <template>
   <!--For testing only-->
   <h1 style="position: absolute; top: 0; z-index: 100;">
-    <button @click="selectedPack = null">Reset</button>
+    <button @click="reset">Reset</button>
   </h1>
   <div>
     <div v-if="!selectedPack" class="packs-container">
       <div v-for="pack in packs" :key="pack.id">
-        <CardPack :packId="pack.id" @open-animation-ended="displayCards(pack.id)"></CardPack>
+        <CardPack v-if="pack.visible" :packId="pack.id" :class="{ 'fade-out': pack.fadeOut }"
+          @click="hideOtherPacks(pack.id)" @open-animation-ended="displayCards(pack.id)" />
       </div>
     </div>
     <div v-if="selectedPack" class="card-container">
@@ -34,13 +35,32 @@ export default {
       packs: packsData.map(item => {
         return {
           ...item,
+          visible: true
         };
       }),
       selectedPack: null,
     };
   },
-
   methods: {
+    reset() {
+      this.packs.forEach((pack) => {
+        pack.visible = true;
+        pack.fadeOut = false;
+      })
+      this.selectedPack = null;
+    },
+    hideOtherPacks(packId) {
+      this.packs.forEach((pack) => {
+        if (pack.id === packId) {
+          pack.centered = true;
+        } else {
+          pack.fadeOut = true;
+          setTimeout(() => {
+            pack.visible = false;
+          }, 500);
+        }
+      });
+    },
     displayCards(packId) {
       const pack = this.packs.find(p => p.id === packId);
       this.selectedPack = pack;
@@ -63,6 +83,11 @@ export default {
   display: flex;
   justify-content: space-around;
   gap: 20px;
+}
+
+.fade-out {
+  transition: opacity 0.5s ease-out;
+  opacity: 0;
 }
 </style>
 
